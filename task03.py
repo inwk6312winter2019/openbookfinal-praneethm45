093a095ecc387b5e6f4e4eb43ca4b823d4b7e650
praneethm45 <praneethmaram55@gmail.com>
2019-02-15 11:28:15 -0800

    trial

---
+++ task03.py
@@ -0,0 +1,15 @@
+import create-ticket.py
+import requests
+import json
+def GetToken():
+    url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    data = {}
+    data['username'] = "devnetuser"
+    data['password'] = "Cisco123!"
+    data= json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=data,headers=headers)
+    return response.json()
+
+print(GetToken())
+
